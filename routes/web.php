8395e3efad01bb76b42c8e97f8e463b652b50e01
fardani fatimazahra <fardani@webatis.com>
2025-02-09 23:33:11 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ Route::get('/contacts',[ContactController::class,'contacts']);
+
