58689f72a0075a78e6e27774393c6e50fc3d288f
fardani fatimazahra <fardani@webatis.com>
2025-02-28 00:24:12 +0100

    commit for cros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\VerifyCsrfToken;
+use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(EncryptCookies::class);
-        $middleware->append(StartSession::class);
+        $middleware->append(CorsMiddleware::class);
         $middleware->append(VerifyCsrfToken::class);
+       
     })
     
     
